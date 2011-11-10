ab6b109bb9e04434b0c0953d011d07c3f95185b8
Anis Kadri <anis.kadri@gmail.com>
2011-11-10 11:54:36 -0800

    adding search to cache

--- assets/www/js/search.js
+++ assets/www/js/search.js
@@ -4,95 +4,95 @@ function search(isSuggestion) {
         $('#search').removeClass('inProgress');
         return;
     }
-	if (hasNetworkConnection()) {
-		var searchParam = $('#searchParam').val();
-	
-		if (searchParam == '') {
-			hideOverlays();
-			return;
-		}
-		
-		showSpinner();
+    if (hasNetworkConnection()) {
+        var searchParam = $('#searchParam').val();
+    
+        if (searchParam == '') {
+            hideOverlays();
+            return;
+        }
+        
+        showSpinner();
         toggleProgress();
 
-		var requestUrl = currentLocale.url + "/w/api.php?action=opensearch&";
-		requestUrl += "search=" + encodeURIComponent(searchParam) + "&";
-		requestUrl += "format=json";
+        var requestUrl = currentLocale.url + "/w/api.php?action=opensearch&";
+        requestUrl += "search=" + encodeURIComponent(searchParam) + "&";
+        requestUrl += "format=json";
         
         if (!isSuggestion) {
-		  $.ajax({
-			  type:'Get',
-			  url:requestUrl,
-			  success:function(data) {
-			     displayResults(data);
-		      }
-		  });
-		}else{
+          $.ajax({
+              type:'Get',
+              url:requestUrl,
+              success:function(data) {
+                 displayResults(data);
+              }
+          });
+        }else{
           $.ajax({
               type:'Get',
               url:requestUrl,
               success:function(data) {
                  displaySuggestions(data);
               }
-          });		
-		}
-	}else{
-		noConnectionMsg();
-		hideOverlays();
-	}
+          });       
+        }
+    }else{
+        noConnectionMsg();
+        hideOverlays();
+    }
 }
 
 function displayResults(results) {
     setActiveState();
-	var formattedResults = "";
-	
-	if (results != null) {
-		results = JSON.parse(results);
-	
-		if (results.length > 0) {
-			var searchParam = results[0];
-			var searchResults = results[1];
-		
-			for (var i=0;i<searchResults.length;i++) {
-				var article = searchResults[i];
-				
-				if (article.toLowerCase() == $('#searchParam').val().toLowerCase()) {
-					goToResult(article);
-					return;
-				}
-				
-				formattedResults += "<div class='listItemContainer' onclick=\"javascript:goToResult(\'" + article + "\');\">";
-				formattedResults += "<div class='listItem'>";
-				formattedResults += "<span class='iconSearchResult'></span>";
-				formattedResults += "<span class='text'>" + article + "</span>";
-				formattedResults += "</div>";
-				formattedResults += "</div>";
-			}
-		}else{
-		    formattedResults += "<div class='listItemContainer'>";
+    var formattedResults = "";
+    
+    if (results != null) {
+        results = JSON.parse(results);
+    
+        if (results.length > 0) {
+            var searchParam = results[0];
+            var searchResults = results[1];
+        
+            for (var i=0;i<searchResults.length;i++) {
+                var article = searchResults[i];
+                
+                if (article.toLowerCase() == $('#searchParam').val().toLowerCase()) {
+                    goToResult(article);
+                    return;
+                }
+                
+                formattedResults += "<div class='listItemContainer' onclick=\"javascript:goToResult(\'" + article + "\');\">";
+                formattedResults += "<div class='listItem'>";
+                formattedResults += "<span class='iconSearchResult'></span>";
+                formattedResults += "<span class='text'>" + article + "</span>";
+                formattedResults += "</div>";
+                formattedResults += "</div>";
+            }
+        }else{
+            formattedResults += "<div class='listItemContainer'>";
             formattedResults += "<div class='listItem'>";
             formattedResults += "<span class='iconSearchResult'></span>";
             formattedResults += "<span class='text'>No results found</span>";
             formattedResults += "</div>";
             formattedResults += "</div>";
-		}
-	}else{
+        }
+    }else{
         // no result from the server...
-	}
-	
-	formattedResults += "<div class='listItemContainer' onclick='javascript:hideOverlays();'>";
-	formattedResults += "<div class='listItem'>Close</div>";
-	formattedResults += "</div>";
-	
-	$('#resultList').html(formattedResults);
+    }
+    
+    formattedResults += "<div class='listItemContainer' onclick='javascript:hideOverlays();'>";
+    formattedResults += "<div class='listItem'>Close</div>";
+    formattedResults += "</div>";
+    
+    $('#resultList').html(formattedResults);
 
     toggleProgress();
     hideSpinner();
-	hideOverlays();
+    hideOverlays();
 
-	$('#searchresults').show();
-	$('#content').hide();
-	
+    $('#searchresults').show();
+    $('#content').hide();
+    
 }
 
 function displaySuggestions(results) {
@@ -144,25 +144,26 @@ function displaySuggestions(results) {
 }
 
 function goToResult(article) {
-	if (hasNetworkConnection()) {
-	    showSpinner();
+    if (hasNetworkConnection()) {
+        showSpinner();
         toggleProgress();
-		var url = currentLocale.url + "/wiki/" + article;	
-		$('#main').attr('src', url);
-		hideOverlays();
-	}else{
-		noConnectionMsg();
-	}
+        var url = currentLocale.url + "/wiki/" + article;   
+        app.loadAndCachePage(url);
+//      $('#main').attr('src', url);
+        hideOverlays();
+    }else{
+        noConnectionMsg();
+    }
 }
 
 function showSpinner() {
-	$('.titlebar .spinner').css({display:'block'});
-	$('#clearSearch').css({height:0});
+    $('.titlebar .spinner').css({display:'block'});
+    $('#clearSearch').css({height:0});
 }
 
 function hideSpinner() {
-	$('.titlebar .spinner').css({display:'none'});	
-	$('#clearSearch').css({height:30});
+    $('.titlebar .spinner').css({display:'none'});  
+    $('#clearSearch').css({height:30});
 }
 
 function toggleProgress() {
