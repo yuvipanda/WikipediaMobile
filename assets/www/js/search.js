036d88529225f48973421a07d2b292c4ce24f10e
YuviPanda <yuvipanda@gmail.com>
2011-12-23 05:07:35 +0530

    Search suggestions + DYM integration

--- assets/www/js/search.js
+++ assets/www/js/search.js
@@ -15,45 +15,50 @@ function search(isSuggestion) {
 		showSpinner();
 		$('#search').addClass('inProgress');
 
-		if(!isSuggestion) {
-			var url = urlForTitle(searchParam);
-			goToResult(url);
-			return;
-		}
-
 		var requestUrl = currentLocale.url + "/w/api.php";
-		$.ajax({
-			type: 'GET',
-			url: requestUrl,
-			data: {
-				action: 'opensearch',
+		if(isSuggestion) {
+			$.ajax({
+				type: 'GET',
+				url: requestUrl,
+				data: {
+					action: 'opensearch',
 				search: searchParam,
 				format: 'json'
-			},
-			success: function(data) {
-				displayResults(data, isSuggestion);
-			}
-		});
+				},
+				success: function(data) {
+					displayResults(JSON.parse(data), isSuggestion);
+				}
+			});
+		} else {
+			goToResult(urlForTitle(searchParam));
+		}
 	} else {
 		noConnectionMsg();
 		hideOverlays();
 	}
 }
 
+function adaptQueryToSearch(queryResults, searchParam) {
+	var searchResults = [];
+	for(var i = 0; i < queryResults.query.search.length; i++) {
+		var result = queryResults.query.search[i];
+		searchResults.push(result.title);
+	}
+	return [searchParam, searchResults];
+}
+
 function urlForTitle(title) {
     return currentLocale.url + "/wiki/" + encodeURIComponent(title.replace(/ /g, '_'));
 }
 
-function displayResults(results, isSuggestion) {
+function displayResults(results, isSuggestion, isDYM) {
 	setActiveState();
 	var formattedResults = "";
 
 	if (results != null) {
-		results = JSON.parse(results);
-
-		if (results.length > 0) {
-			var searchParam = results[0];
-			var searchResults = results[1];
+		var searchParam = results[0];
+		var searchResults = results[1];
+		if (searchResults.length > 0) {
 
 			for (var i=0;i<searchResults.length;i++) {
 				var article = searchResults[i];
@@ -67,12 +72,28 @@ function displayResults(results, isSuggestion) {
 				formattedResults += "</div>";
 			}
 		} else {
-			formattedResults += "<div class='listItemContainer'>";
-			formattedResults += "<div class='listItem'>";
-			formattedResults += "<span class='iconSearchResult'></span>";
-			formattedResults += "<span class='text'>No results found</span>";
-			formattedResults += "</div>";
-			formattedResults += "</div>";
+			if(isDYM) {
+				// Well, already in DYM, and nothing found
+				formattedResults += "<div><span class=\'text\'>No results found</span></div>";
+			} else {
+				// We are in suggestions, and nothing is found
+				var requestUrl = currentLocale.url + "/w/api.php";
+				$.ajax({
+					type: 'GET',
+					url: requestUrl,
+					data: {
+						action: 'query',
+						list: 'search',
+						srsearch: searchParam,
+						srinfo: 'suggestion',
+						format: 'json'
+					},
+					success: function(data) {
+						displayResults(adaptQueryToSearch(JSON.parse(data), searchParam), isSuggestion, true);
+					}
+				});
+			}
+			return;
 		}
 	} else {
 		// no result from the server...
