4e2d2656ae733bc3cd1b76b9d25d211264ea3633
YuviPanda <yuvipanda@gmail.com>
2012-01-17 16:18:14 -0800

    Fixed a bunch of silly typos

--- assets/www/js/chrome.js
+++ assets/www/js/chrome.js
@@ -243,7 +243,7 @@ window.chrome = function() {
 
 			if (url.match(/^https?:\/\/([^\/]+)\.wikipedia\.org\/wiki\//)) {
 				// ...and load it through our intermediate cache layer.
-				navigateToPage(url);
+				app.navigateToPage(url);
 			} else {
 				// ...and open it in parent context for reals.
 				//
