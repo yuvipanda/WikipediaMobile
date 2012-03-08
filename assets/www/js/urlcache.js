6e74db80bd3d041eafaaedf080fa12b5265daea3
YuviPanda <yuvipanda@gmail.com>
2012-03-09 02:14:57 +0530

    Remove some unusually huge console.log output
    It was basically outputting entire HTML of pages

--- assets/www/js/urlcache.js
+++ assets/www/js/urlcache.js
@@ -5,7 +5,6 @@ window.urlCache = function() {
 
 	function dataUrlForImage(img) {
 		var d = $.Deferred();
-		console.log("Starting canvas!");	
 		// Create an empty canvas element
 		var canvas = document.createElement("canvas");
 		canvas.width = img.width;
@@ -33,9 +32,7 @@ window.urlCache = function() {
 				$.get(url, function(data) {
 					fileEntry.createWriter(function(writer) {
 						writer.write(data);
-						console.log("Writing stuff to " + fileEntry.fullPath);
 						writer.onwriteend = function() {
-							console.log("written stuff!");
 							d.resolve(fileEntry.fullPath);
 						};
 					});
@@ -48,13 +45,10 @@ window.urlCache = function() {
 			return d;
 		}
 
-		console.log("Trying to open " + filePath);
 		window.resolveLocalFileSystemURI(filePath,
 				function(fileEntry) {
-					console.log("Found!");
 					d.resolve(fileEntry.fullPath);
 				}, function(error) {
-					console.log("Not found!");
 					cacheUrl();
 				}
 		);
@@ -68,22 +62,16 @@ window.urlCache = function() {
 		var fileName = Crypto.MD5(url);
 		var filePath = fileName;
 
-		console.log("Starting to save");
 		var element = $(html);
 		var replacements = {};
-		console.log("HTML Parsed");
 		function saveFile(fileEntry) {
 			fileEntry.createWriter(function(writer) {
 				writer.write(html);
-				console.log('html is ' + html);
-				console.log("Writing stuff to " + fileEntry.fullPath);
 				writer.onwriteend = function() {
-					console.log("written stuff!");
 					d.resolve(fileEntry.fullPath);
 				};
 			});
 		}
-		console.log("About to map stuff");
 
 		// Incredibly wasteful hack going to happen. I'm sorry
 		// I am parsing the entire HTML again, *and* doing string replacement
@@ -97,15 +85,10 @@ window.urlCache = function() {
 			html = html.replace(href, data);
 		});
 
-		console.log("Done mapping stuff");
-		console.log("Inside the when");
 		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
 			function(fs){
-				console.log(filePath);
-				console.log(JSON.stringify(fs));
 				fs.root.getFile(filePath, {create: true}, saveFile, error);
 			});
-		console.log("Technically done");
 		return d;
 	}
 
@@ -119,10 +102,8 @@ window.urlCache = function() {
 		function readFile(fileEntry) {
 			var reader = new FileReader();
 			reader.onloadend = function(evt) {
-				console.log(JSON.stringify(evt));
 				d.resolve(evt.target.result);
 			};
-			console.log('file path is ' + JSON.stringify(fileEntry));
 			fileEntry.file(function(file) {reader.readAsText(file); });
 		}
 		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
