09f6a49476d7610792059dbcf705d9e7bc55002b
tpt <thomaspt@hotmail.fr>
2012-08-20 13:47:07 +0200

    Remove of old ressources that were used by google map based "near me" feature

--- src/org/wikipedia/WikipediaActivity.java
+++ src/org/wikipedia/WikipediaActivity.java
@@ -3,7 +3,6 @@ package org.wikipedia;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.util.Log;
 
 import org.apache.cordova.DroidGap;
 
